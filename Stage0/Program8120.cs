a4040004d46d0563954016fb65ec2c087a6da8a4
oritvladi <oritvladi@gmail.com>
2023-10-23 00:37:18 +0300

    initial code+ wellcome func

---
+++ Stage0/Program8120.cs
@@ -0,0 +1,18 @@
+﻿using System;
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome8120();
+        Welcome8674();
+        Console.ReadKey();
+    }
+
+    private static void Welcome8120()
+    {
+        Console.WriteLine("Enter Your Name: ");
+        string name = System.Console.ReadLine();
+        Console.WriteLine("{0}, wellcome to my first console application", name);
+    }
+    static partial void Welcome8674();
+}
